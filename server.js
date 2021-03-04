be96287bbd566b85074f06e6e878702051ec6a58
msolorio <msolorio.github@gmail.com>
2021-03-04 08:28:32 -0800

    Adds env var for port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const {
   citiesRoutes,
   postsRoutes
